81544200936491f4d19d3cdf235dd9f58653e478
Kathe <ft.katheochoa@gmail.com>
2020-05-17 18:06:02 -0400

    Lab intro-Node

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((item1, item2) => {
+      return item1 - item2;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) throw new Error("OutOfBounds");
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) throw new Error("EmptySortedList");
 
-  min() {}
+    return Math.max(...this.items);
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0) throw new Error("EmptySortedList");
 
-  avg() {}
+    return Math.min(...this.items);
+  }
+
+  sum() {
+    let sum = 0;
+    for (let item of this.items) {
+      sum += item;
+    }
+    return sum;
+  }
+
+  avg() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    let sum = 0;
+    for (let item of this.items) {
+      sum += item;
+    }
+    return sum / this.length;
+  }
 }
 
 module.exports = SortedList;
